d752b198c9b293ef839ef76bd04eb25cb8a41b90
Abdülhamit Yilmaz <mr.yilmaz@gmx.de>
2014-04-29 14:04:43 +0200

    A little bit refactoring with the help of Pylint

--- __init__.py
+++ __init__.py
@@ -4,8 +4,8 @@ from PyQt4.QtGui import QApplication
 from ui.main import Main
 
 if __name__ == "__main__":
-    from sys import argv, exit
-    app = QApplication(argv)
-    ui = Main()
-    ui.show()
-    exit(app.exec_())
+    from sys import argv
+    APP = QApplication(argv)
+    UI = Main()
+    UI.show()
+    exit(APP.exec_())
--- ui/main.py
+++ ui/main.py
@@ -1,39 +1,21 @@
 # -*- coding: utf-8 -*-
 
-"""
-Module implementing Main.
-"""
-
 from PyQt4.QtGui import QMainWindow
-from PyQt4.QtCore import pyqtSignature, QProcess
+from PyQt4.QtCore import pyqtSignature
 
-from Ui_main import Ui_MainWindow,  _translate
+from lib import process_command
+from ui.Ui_main import Ui_MainWindow
 
 
 class Main(QMainWindow, Ui_MainWindow):
-    """
-    Class documentation goes here.
-    """
     def __init__(self, parent=None):
-        """
-        Constructor
-        """
         QMainWindow.__init__(self, parent)
         self.setupUi(self)
 
     @pyqtSignature("")
     def on_pushButton_clicked(self):
-        """
-        Slot documentation goes here.
-        """
-        process = QProcess(self)
-        process.start(self.lineEdit.text())
-        process.waitForFinished()
-        self.textBrowser.setText(process.readAllStandardOutput().data())
+        self.textBrowser.setText(process_command(self, self.lineEdit.text()))
 
     @pyqtSignature("")
     def on_lineEdit_returnPressed(self):
-        """
-        Slot documentation goes here.
-        """
         self.on_pushButton_clicked()

a82e8ab81e900c58e937be5543b2d48f21f847ec
Abdülhamit Yilmaz <mr.yilmaz@gmx.de>
2014-05-01 11:31:31 +0200

    Clean up process_command
    Looks like QProcess doesn't need a parent to work like intended.

--- helper/__init__.py
+++ helper/__init__.py
@@ -4,8 +4,8 @@
 from PyQt4.QtCore import QProcess
 
 
-def process_command(q_parent, command):
-    process = QProcess(q_parent)
+def process_command(command):
+    process = QProcess()
     process.start(command)
     process.waitForFinished()
     return process.readAllStandardOutput().data()

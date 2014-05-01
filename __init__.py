afc4ac58e06fdb505b5cfaf6a7c02ea00e669dbf
Abdülhamit Yilmaz <mr.yilmaz@gmx.de>
2014-05-01 11:30:28 +0200

    Clean up main file
    if __name__ == "__main__": isn't necessary

--- __init__.py
+++ __init__.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from sys import argv
+
 from PyQt4.QtGui import QApplication
+
 from ui.main import Main
 
-if __name__ == "__main__":
-    from sys import argv
-    APP = QApplication(argv)
-    UI = Main()
-    UI.show()
-    exit(APP.exec_())
+
+APP = QApplication(argv)
+UI = Main()
+UI.show()
+exit(APP.exec_())

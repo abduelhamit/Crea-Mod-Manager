75618718f8bf3db1848a5a1721f2d9ccbb846963
Abdülhamit Yilmaz <mr.yilmaz@gmx.de>
2014-05-02 11:03:49 +0200

    Make ui.show_cmf_info to mod_file.parse_info

--- mod_file/__init__.py
+++ mod_file/__init__.py
@@ -15,3 +15,43 @@ def load_info(path):
         xml = process_command(u"tools/7za e -so {} info.xml".format(path))
     cmf = objectify.fromstring(xml, parser)
     return cmf
+
+def parse_info(path):
+    mod = load_info(path)
+    display_text = []
+    display_text += mod.name.text
+    display_text += " (" + mod.version.get("format").format(
+        *mod.version.getchildren()) + ")"
+    display_text += " by " + mod.author.text
+    display_text += "\n\n" + mod.shortDesc.text
+    display_text += "\n\n" + mod.homepage.text
+    display_text += "\n\n----------------------------------------"
+    display_text += "---------------------------------------"
+    display_text += "\n\n" + mod.desc.text
+    display_text += "\n\n----------------------------------------"
+    display_text += "---------------------------------------"
+    display_text += "\n\nChangelog:"
+    for entry in mod.changelog.entry:
+        display_text += "\n\n{} ({}):\n{}".format(
+            entry.get("version"), entry.get("date"), entry.text)
+    display_text += "\n\n----------------------------------------"
+    display_text += "---------------------------------------"
+    display_text += "\n\nAdds:"
+    try:
+        for add in mod.files.add:
+            display_text += "\n" + add.text
+    except TypeError:
+        display_text += "\nnothing"
+    display_text += "\n\nModifies:"
+    try:
+        for modify in mod.files.modify:
+            display_text += "\n" + modify.text
+    except TypeError:
+        display_text += "\nnothing"
+    display_text += "\n\nReplaces:"
+    try:
+        for replace in mod.files.replace:
+            display_text += "\n" + replace.text
+    except TypeError:
+        display_text += "\nnothing"
+    return "".join(display_text)
--- ui/main.py
+++ ui/main.py
@@ -5,7 +5,7 @@ from lxml.etree import XMLSyntaxError
 from PyQt4.QtGui import QMainWindow, QFileDialog, QMessageBox
 from PyQt4.QtCore import pyqtSignature
 
-from mod_file import load_info
+from mod_file import parse_info
 from ui.Ui_main import Ui_MainWindow
 
 
@@ -13,57 +13,6 @@ class Main(QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         QMainWindow.__init__(self, parent)
         self.setupUi(self)
-
-    def show_cmf_info(self):
-        try:
-            mod = load_info(self.modFile.text())
-        except (XMLSyntaxError) as e:
-            if e.message:
-                message = e.message
-            else:
-                message = "Unknown error"
-            QMessageBox.critical(
-                self, "CMF Parsing Error",
-                "Oops. Something went wrong while opening the CMF:\n{}".format(
-                    message))
-            return
-        display_text = []
-        display_text += mod.name.text
-        display_text += " (" + mod.version.get("format").format(
-            *mod.version.getchildren()) + ")"
-        display_text += " by " + mod.author.text
-        display_text += "\n\n" + mod.shortDesc.text
-        display_text += "\n\n" + mod.homepage.text
-        display_text += "\n\n----------------------------------------"
-        display_text += "---------------------------------------"
-        display_text += "\n\n" + mod.desc.text
-        display_text += "\n\n----------------------------------------"
-        display_text += "---------------------------------------"
-        display_text += "\n\nChangelog:"
-        for entry in mod.changelog.entry:
-            display_text += "\n\n{} ({}):\n{}".format(
-                entry.get("version"), entry.get("date"), entry.text)
-        display_text += "\n\n----------------------------------------"
-        display_text += "---------------------------------------"
-        display_text += "\n\nAdds:"
-        try:
-            for add in mod.files.add:
-                display_text += "\n" + add.text
-        except TypeError:
-            display_text += "\nnothing"
-        display_text += "\n\nModifies:"
-        try:
-            for modify in mod.files.modify:
-                display_text += "\n" + modify.text
-        except TypeError:
-            display_text += "\nnothing"
-        display_text += "\n\nReplaces:"
-        try:
-            for replace in mod.files.replace:
-                display_text += "\n" + replace.text
-        except TypeError:
-            display_text += "\nnothing"
-        self.out.setText("".join(display_text))
 
     @pyqtSignature("")
     def on_lineEdit_returnPressed(self):
@@ -75,7 +24,19 @@ class Main(QMainWindow, Ui_MainWindow):
 
     @pyqtSignature("")
     def on_modFile_returnPressed(self):
-        self.show_cmf_info()
+        try:
+            info_text = parse_info(self.modFile.text())
+        except (XMLSyntaxError) as e:
+            if e.message:
+                message = e.message
+            else:
+                message = "Unknown error"
+            QMessageBox.critical(
+                self, "CMF Parsing Error",
+                "Oops. Something went wrong while opening the CMF:\n{}".format(
+                    message))
+            return
+        self.out.setText(info_text)
 
     @pyqtSignature("")
     def on_modFileButton_clicked(self):
@@ -83,8 +44,20 @@ class Main(QMainWindow, Ui_MainWindow):
             self, filter="Crea Mod File (*.cmf)")
         if not file_name:
             return
+        try:
+            info_text = parse_info(file_name)
+        except (XMLSyntaxError) as e:
+            if e.message:
+                message = e.message
+            else:
+                message = "Unknown error"
+            QMessageBox.critical(
+                self, "CMF Parsing Error",
+                "Oops. Something went wrong while opening the CMF:\n{}".format(
+                    message))
+            return
+        self.out.setText(info_text)
         self.modFile.setText(file_name)
-        self.show_cmf_info()
 
     @pyqtSignature("")
     def on_creaPath_returnPressed(self):

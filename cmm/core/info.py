c661fe2f6823bab7c7ffd7bf3780964dd261e7f9
Abdülhamit Yilmaz <mr.yilmaz@gmx.de>
2014-05-17 16:43:47 +0200

    Add parser in Info

--- cmm/core/info.py
+++ cmm/core/info.py
@@ -3,8 +3,22 @@
 '''The Info class.
 Every interaction with an info.xml file happens over a Info object.'''
 
+from lxml import etree, objectify
+
 
 class Info:
     '''The Info class.'''
     def __init__(self):
-        pass
+        self.xml = None
+
+        with open("cmm/core/info.xsd") as schema_file:
+            schema = etree.XMLSchema(file=schema_file)
+        self.parser = objectify.makeparser(schema=schema)
+
+    def parse_string(self, xml):
+        '''Parse a string.'''
+        self.xml = objectify.fromstring(xml, self.parser)
+
+    def parse_file(self, xml):
+        '''Parse a file or file-like object.'''
+        self.xml = objectify.parse(xml, self.parser)

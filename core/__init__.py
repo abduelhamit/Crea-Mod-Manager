086799a2bf23b4d13374f69b22b229a400352fb6
Abdülhamit Yilmaz <mr.yilmaz@gmx.de>
2014-05-17 10:40:09 +0200

    Make the folders to Python packages

---
+++ core/__init__.py
@@ -0,0 +1,3 @@
+# coding=utf-8
+
+'''The core of CMM. It is distributed under the zlib license.'''
---
+++ ui/__init__.py
@@ -0,0 +1,3 @@
+# coding=utf-8
+
+'''The UI of CMM. It is distributed under the GNU GPL v3 or later.'''

f09ec1128bcb9b9cec7b5fb666a0fe5c83eac50b
Abdülhamit Yilmaz <mr.yilmaz@gmx.de>
2014-05-17 19:06:58 +0200

    Improve Info constructor

--- cmm/core/info.py
+++ cmm/core/info.py
@@ -9,16 +9,16 @@ from lxml import etree, objectify
 class Info:
     '''The Info class.'''
     def __init__(self):
-        self.xml = None
-
         with open("cmm/core/info.xsd") as schema_file:
             schema = etree.XMLSchema(file=schema_file)
-        self.parser = objectify.makeparser(schema=schema)
+        objectify.set_default_parser(objectify.makeparser(schema=schema))
+
+        self.xml = objectify.Element("cmf", attrib={"version": "0"})
 
     def parse_string(self, xml):
         '''Parse a string.'''
-        self.xml = objectify.fromstring(xml, self.parser)
+        self.xml = objectify.fromstring(xml)
 
     def parse_file(self, xml):
         '''Parse a file or file-like object.'''
-        self.xml = objectify.parse(xml, self.parser)
+        self.xml = objectify.parse(xml)

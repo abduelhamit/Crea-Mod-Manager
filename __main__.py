4f6a3c82d82f7a45f11853468bd0588c66efd951
Abdülhamit Yilmaz <mr.yilmaz@gmx.de>
2014-05-17 09:58:37 +0200

    Cleaning up

--- __main__.py
+++ __main__.py
@@ -1,14 +1,2 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
-
-from sys import argv
-
-from PyQt4.QtGui import QApplication
-
-from ui.main import Main
-
-
-APP = QApplication(argv)
-UI = Main()
-UI.show()
-exit(APP.exec_())

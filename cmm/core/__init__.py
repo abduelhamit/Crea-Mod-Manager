b19ab3d5b89073cc33ac877c6989d94dc0764887
Abdülhamit Yilmaz <mr.yilmaz@gmx.de>
2014-05-17 14:03:36 +0200

    Put Python code in cmm package

--- cmm/core/__init__.py
+++ cmm/core/__init__.py
@@ -1,3 +1,5 @@
 # coding=utf-8
 
 '''The core of CMM. It is distributed under the zlib license.'''
+
+from cmm.core.mod import Mod
---
+++ cmm/core/mod.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+
+'''The Mod class. Every interaction with a CMF happens over a Mod object.'''
+
+
+class Mod:
+    '''The Mod class.'''
+    def __init__(self):
+        pass
